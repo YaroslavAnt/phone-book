6fc4eb2608e52ed9c54cf4863eeea61e09d26023
YaroslavAnt <antonchyk.yar@gmail.com>
2018-05-19 01:14:51 +0300

    done with form validation

--- src/containers/add-contact.js
+++ src/containers/add-contact.js
@@ -11,24 +11,36 @@ export class AddContact extends React.Component{
     e.preventDefault();
     const form=document.getElementById('form'),
           firstName=form.firstName,
-          secondName=form.secondName.value,
-          email=form.email.value,
-          phone=form.phone.value;
+          secondName=form.secondName,
+          email=form.email,
+          btn=form.btn,
+          phone=form.phone;
 
-    if(/[^a-z]/.test(firstName.value)) alert('First name should contain only letters')
-    else if(!secondName.match(/[a-z]/gi)) alert('Second name should contain only letters')
-    else if(!email.match(/[a-z]/gi)) alert('Email should contain only letters')
-    else if(!phone.match(/[0-9]/gi)) alert('Phone should contain only letters')
+    console.log(btn.className)
+
+    firstName.style.borderColor='green';
+    secondName.style.borderColor='green';
+    email.style.borderColor='green';
+    phone.style.borderColor='green';        
+
+    if(/[^a-z]/gi.test(firstName.value) || firstName.value.length===0) firstName.style.borderColor='red'
+    else if(/[^a-z]/gi.test(secondName.value) || secondName.value.length===0) secondName.style.borderColor='red'
+    else if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(email.value)) email.style.borderColor='red'
+    else if(!/^((8|\+38)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(phone.value)) phone.style.borderColor='red'
+    else {      
+      btn.removeAttribute('disabled');
+      btn.className= btn.className.replace('disabled', '')
+    }
   }
   render(){
     return(
       <Form id="form">
         <img alt="img"/>
-        <Form.Input name="firstName" placeholder="firstName" />
-        <Form.Input name="secondName" placeholder="secondName" />
-        <Form.Input name="email" placeholder="email" />
-        <Form.Input name="phone" placeholder="phone" />
-        <Button type="submit" name="btn"  onClick={this.handleChange} >Add Contact</Button>
+        <Form.Input onChange={this.handleChange} label="First Name" name="firstName" placeholder="First Name" />
+        <Form.Input onChange={this.handleChange} label="Second Name" name="secondName" placeholder="Second Name" />
+        <Form.Input onChange={this.handleChange} label="Email" name="email" placeholder="email@mail.com" />
+        <Form.Input onChange={this.handleChange} label="Phone" name="phone" placeholder="+000(00)000-00-00" />
+        <Button type="submit" name="btn" disabled onClick={this.handleChange} >Add Contact</Button>
       </Form>
     )
   }
